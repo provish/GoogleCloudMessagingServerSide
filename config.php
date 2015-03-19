cacbdae2c483da9519aa136d17918a026bd9c25f
provish <vshlgupta1992@gmail.com>
2015-03-19 20:43:12 +0530

    GoogleCloudMessaging
    GCM Server side implementation.

---
+++ config.php
@@ -0,0 +1,15 @@
+
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_DATABASE", "gcm");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyD8i6lV33uzQt9YjyTX66WYiYgm5o4V4ik"); // Place your Google API Key
+?>
